e58aa7a8d32bfda27d3316fd974e18f698da45f7
chiragthesia <harithesia@gmail.com>
2021-03-03 10:34:20 -0600

    Sorting instructions

--- src/components/TableContainer.js
+++ src/components/TableContainer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useTable, useSortBy } from 'react-table';
 import { Table } from 'reactstrap';
+import './tableContainer.css';
 
 const TableContainer = ({ columns, data }) => {
 	const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable(
@@ -12,35 +13,38 @@ const TableContainer = ({ columns, data }) => {
 	);
 
 	return (
-		<Table bordered hover {...getTableProps()}>
-			<thead>
-				{headerGroups.map((headerGroup) => (
-					<tr {...headerGroup.getHeaderGroupProps()}>
-						{headerGroup.headers.map((column) => (
-							<th {...column.getHeaderProps(column.getSortByToggleProps())}>
-								{column.render('Header')}
-								<span>
-									{column.isSorted ? column.isSortedDesc ? '👇' : '👆' : ''}
-								</span>
-							</th>
-						))}
-					</tr>
-				))}
-			</thead>
-
-			<tbody {...getTableBodyProps()}>
-				{rows.map((row) => {
-					prepareRow(row);
-					return (
-						<tr {...row.getRowProps()}>
-							{row.cells.map((cell) => {
-								return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>;
-							})}
+		<div>
+			<p className="instruction">You can sort each column by clicking on the title</p>
+			<Table bordered hover {...getTableProps()}>
+				<thead>
+					{headerGroups.map((headerGroup) => (
+						<tr {...headerGroup.getHeaderGroupProps()}>
+							{headerGroup.headers.map((column) => (
+								<th {...column.getHeaderProps(column.getSortByToggleProps())}>
+									{column.render('Header')}
+									<span>
+										{column.isSorted ? column.isSortedDesc ? '👇' : '👆' : ''}
+									</span>
+								</th>
+							))}
 						</tr>
-					);
-				})}
-			</tbody>
-		</Table>
+					))}
+				</thead>
+
+				<tbody {...getTableBodyProps()}>
+					{rows.map((row) => {
+						prepareRow(row);
+						return (
+							<tr {...row.getRowProps()}>
+								{row.cells.map((cell) => {
+									return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>;
+								})}
+							</tr>
+						);
+					})}
+				</tbody>
+			</Table>
+		</div>
 	);
 };
 
