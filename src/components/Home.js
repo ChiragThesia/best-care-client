db8e7ef70a8d816bad1fb9be151fe3ce4e1dfec9
chiragthesia <harithesia@gmail.com>
2021-03-04 15:48:57 -0600

    added image to home pge

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,10 @@ function Home() {
 	return (
 		<div className="homeContainer">
 			<h1>Welcome to Beyond Care</h1>
+			<img
+				alt="doc"
+				src="https://res.cloudinary.com/dnsl4nbz4/image/upload/v1614894503/Screen_Shot_2021-03-04_at_3.45.28_PM_zfms6j.png"
+			/>
 		</div>
 	);
 }
